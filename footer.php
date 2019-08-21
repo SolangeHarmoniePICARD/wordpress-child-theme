6a026fcf67ed41564f1c15bca2f7c2667af140ca
Solange Harmonie PICARD <solange.picard@cryptolab.net>
2019-08-21 16:00:34 +0200

    Le thème enfant que j'utilise pour mon blog.

---
+++ footer.php
@@ -0,0 +1,12 @@
+		<div class="left-sidebar sidebar-mobile">
+			<?php get_sidebar( 'mobile' ); ?>
+		</div>
+
+		<div class="clearfix"></div>
+		<footer>
+					<span class="alignleft">&copy; <?php echo date( 'Y' ) ?> <a href="<?php echo home_url(); ?>" title="<?php bloginfo( 'name' ); ?>"><?php bloginfo( 'name' ); ?></a></span><br\>
+					<span class="alignright">Theme by <a rel="nofollow" target="_blank" href="https://solangeharmoniepicard.fr/">Solange Harmonie PICARD</a></span>
+					</br\></footer>
+	<?php wp_footer(); ?>
+	</body>
+</html>
