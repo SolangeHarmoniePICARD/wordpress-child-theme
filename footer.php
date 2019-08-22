bfa8392852f9faba33d93a7b9582e059734e48a6
Solange Harmonie PICARD <solange.picard@cryptolab.net>
2019-08-22 16:15:34 +0200

    CSS (et HTML) de la « Flip card ».

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 		<div class="clearfix"></div>
 		<footer>
 					<span class="alignleft">&copy; <?php echo date( 'Y' ) ?> <a href="<?php echo home_url(); ?>" title="<?php bloginfo( 'name' ); ?>"><?php bloginfo( 'name' ); ?></a></span><br\>
-					<span class="alignright">Theme by <a rel="nofollow" target="_blank" href="https://solangeharmoniepicard.fr/">Solange Harmonie PICARD</a></span>
+					<span class="alignright">Thème par <a rel="nofollow" target="_blank" href="https://solangeharmoniepicard.fr/">Solange Harmonie PICARD</a></span>
 					</br\></footer>
 	<?php wp_footer(); ?>
 	</body>
